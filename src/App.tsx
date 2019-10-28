a665b4ccfa97f8b1c1904877206e6c3e2e6dccdc
Marc <marc@marmar.io>
2019-10-27 21:28:49 -0700

    added helmet for meta data management

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import Homepage from './Homepage';
-import './App.css';
-import 'semantic-ui-css/semantic.min.css'
+import React from "react";
+import logo from "./logo.svg";
+import Homepage from "./Homepage";
+import Helmet from "react-helmet";
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>The Village Highschool</title>
+        <link rel="canonical" href="http://thevillagehs.org" />
+      </Helmet>
       <Homepage />
     </div>
   );
-}
+};
 
 export default App;
