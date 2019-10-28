9effb83e1521d22ff6d889024206c04578822cf9
Marc <marc@marmar.io>
2019-10-27 17:40:00 -0700

    Updates

---
+++ precache-manifest.58e7d5663ce7a8a3e4f9c95476a8aea3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ce51cd44c76c1a3f4e79e6ae46a225",
+    "url": "/thevillagehs-site/index.html"
+  },
+  {
+    "revision": "f1659ed0ce0ce32f1233",
+    "url": "/thevillagehs-site/static/css/2.e4b89ac4.chunk.css"
+  },
+  {
+    "revision": "ed7b9adedb2435eb6a9f",
+    "url": "/thevillagehs-site/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "f1659ed0ce0ce32f1233",
+    "url": "/thevillagehs-site/static/js/2.30b6d6d1.chunk.js"
+  },
+  {
+    "revision": "ed7b9adedb2435eb6a9f",
+    "url": "/thevillagehs-site/static/js/main.bac0afab.chunk.js"
+  },
+  {
+    "revision": "657712f62391ee643036",
+    "url": "/thevillagehs-site/static/js/runtime-main.b5b8692c.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/thevillagehs-site/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/thevillagehs-site/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/thevillagehs-site/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/thevillagehs-site/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/thevillagehs-site/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/thevillagehs-site/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/thevillagehs-site/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/thevillagehs-site/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/thevillagehs-site/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/thevillagehs-site/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/thevillagehs-site/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/thevillagehs-site/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/thevillagehs-site/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/thevillagehs-site/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/thevillagehs-site/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/thevillagehs-site/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
